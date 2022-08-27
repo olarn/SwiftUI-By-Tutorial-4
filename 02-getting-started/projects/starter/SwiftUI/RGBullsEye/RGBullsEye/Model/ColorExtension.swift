b3784febf60bac3c87a78fbf2b2f644031879d62
Olarn U <olarn.u@odds.team>
2022-08-27 20:00:01 +0700

    Fix code indentation.

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/Model/ColorExtension.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/Model/ColorExtension.swift
@@ -34,10 +34,10 @@ import Foundation
 import SwiftUI
 
 extension Color {
-  /// Create a Color view from an RGB object.
-  ///   - parameters:
-  ///     - rgb: The RGB object.
-  init(rgbStruct rgb: RGB) {
-    self.init(red: rgb.red, green: rgb.green, blue: rgb.blue)
-  }
+    /// Create a Color view from an RGB object.
+    ///   - parameters:
+    ///     - rgb: The RGB object.
+    init(rgbStruct rgb: RGB) {
+        self.init(red: rgb.red, green: rgb.green, blue: rgb.blue)
+    }
 }
