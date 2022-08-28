80d4525bbf716e4e504fbbc08e0412f54636f493
Olarn U <olarn.u@odds.team>
2022-08-28 08:56:58 +0700

    Add landmark project (Apple tutorial) and complete first view tutorial.

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Olarn U. on 28/8/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Olarn U. on 28/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
