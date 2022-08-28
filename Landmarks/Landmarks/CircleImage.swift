7208a7084c53327df303c55f141976b3fe017840
Olarn U <olarn.u@odds.team>
2022-08-28 09:02:00 +0700

    Finish circle image view.

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Olarn U. on 28/8/2565 BE.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
